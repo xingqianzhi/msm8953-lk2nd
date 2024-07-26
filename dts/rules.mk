140a87fc7ea87d3f9369f34f233ac8daf5c7d092
xingqianzhi <98601968+xingqianzhi@users.noreply.github.com>
2024-07-26 16:36:48 +0800

    Update rules.mk

--- dts/rules.mk
+++ dts/rules.mk
@@ -39,7 +39,8 @@ endif
 ifeq ($(PROJECT), msm8917-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8917-lenovo-tbx304.dtb \
-	$(LOCAL_DIR)/msm8917-samsung-r06.dtb
+	$(LOCAL_DIR)/msm8917-samsung-r06.dtb \
+	$(LOCAL_DIR)/msm8917-redmi-4a.dtb
 endif
 ifeq ($(PROJECT), msm8937-secondary)
 DTBS += \
